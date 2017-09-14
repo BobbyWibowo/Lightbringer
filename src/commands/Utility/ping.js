5a3cc520826aa68cb82f5fd24d2b35b4692fa05a
Bobby Wibowo <bobbywibowo98@gmail.com>
2017-09-14 18:46:57 +0700

    Update ping.js

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,6 +1,6 @@
 exports.run = async (bot, msg) => {
   const timestamp = new Date().getTime()
-  await msg.edit('🏓\u2000Pong!')
+  msg = await msg.edit('🏓\u2000Pong!')
   return msg.edit(`${msg.content} - Time taken: **${new Date().getTime() - timestamp}ms** ` +
     `(heartbeat: **${bot.ping.toFixed(0)}ms**).`)
 }
