b3794388be63a645c9fa9cec52bb51e5af60431a
Bobby Wibowo <bobbywibowo98@gmail.com>
2017-10-09 17:41:02 +0700

    Better handling for GIF avatars (avatar command)

--- src/commands/Utility/avatar.js
+++ src/commands/Utility/avatar.js
@@ -1,7 +1,7 @@
 const snekfetch = require('snekfetch')
 
 exports.run = async (bot, msg, args) => {
-  const parsed = bot.utils.parseArgs(args, ['u', 'np'])
+  const parsed = bot.utils.parseArgs(args, 'np')
 
   if (msg.guild && parsed.options.e) {
     bot.utils.assertEmbedPermission(msg.channel, msg.member)
@@ -23,28 +23,14 @@ exports.run = async (bot, msg, args) => {
     avatarURL = avatarURL.replace('cdn.discordapp.com', 'images.discordapp.net')
   }
 
+  if (/\.gif\?size=\d*?$/.test(avatarURL)) {
+    avatarURL += '&f=.gif'
+  }
+
   let message = `${get[1] ? user : user.tag}'s avatar:${parsed.options.e ? '' : `\n${avatarURL}`}` +
     `${member || !msg.guild ? '' : '\n*This user is not a member of the current guild.*'}`
 
-  if (parsed.options.u) {
-    await msg.edit(`${PROGRESS}Fetching avatar of the specified user\u2026`)
-    const res = await snekfetch.get(avatarURL)
-    if (!res || !res.body) {
-      throw new Error('Failed to download avatar of the specified user!')
-    }
-    await msg.channel.send(message.replace(avatarURL, `<${avatarURL}>`), { files: [{
-      attachment: res.body,
-      name: avatarURL.match(/\/([^/?]+)(\?size=\d+)?$/)[1]
-    }] })
-    return msg.delete()
-  } else {
-    if (/\.gif\?size=\d*?$/.test(avatarURL)) {
-      // NOTE: Only show warning if the size paramater is appended to
-      // the URL. If that part isn't in the link, it will play after all.
-      message += '\n*This user\'s avatar is a GIF. Consider using the `-u` option if it doesn\'t play.*'
-    }
-    return msg.edit(message)
-  }
+  return msg.edit(message)
 }
 
 exports.info = {
@@ -54,15 +40,9 @@ exports.info = {
   aliases: ['ava'],
   options: [
     {
-      name: '-u',
-      usage: '-u',
-      description: 'Attempts to upload avatar as an attachment instead (workaround for GIF not playing)'
-    },
-    {
       name: '-np',
       usage: '-np',
-      description: 'Attempts to use the direct URL instead of the proxy URL ' +
-        '(workaround for image sometimes not loading)'
+      description: 'Attempts to use the direct URL instead of the proxy URL (workaround for image sometimes not loading)'
     }
   ]
 }
